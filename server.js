beeabf18f62c99719628557b3972510cc97ed601
ogga hogga <103965225+oggahogga@users.noreply.github.com>
2025-07-30 22:34:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const path = require('path');
+app.use(express.static(path.join(__dirname)));
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
